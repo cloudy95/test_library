06e9df82b6aaa609381e1e78dc5bf82a95bf5dbb
cloudy95 <luissanguino1702@gmail.com>
2025-07-14 11:03:33 -0400

    removiendo codigo comentado

--- src/utils/FormaterCurrency.ts
+++ src/utils/FormaterCurrency.ts
@@ -1,33 +1,6 @@
 import { FormaterCurrencyFnc } from "../models/interfaces";
 import { TYPECURRENZY } from "./const";
 
-// export const FormaterCurrency:any = {
-//     'cl':({currenzy = ''}:FormaterCurrencyFnc)=>{
-//         return formaterCurrencystring({currenzy, decimal:',', miles:'.', contry: 'cl'});
-//     },
-//     'pe':({currenzy = '', typeCurrenzy = '' }:FormaterCurrencyFnc)=>{
-//         if( typeCurrenzy == TYPECURRENZY.euro ){
-//             return formaterCurrencystring({currenzy, decimal:',', miles:'.'});
-//         }
-//         return formaterCurrencystring({currenzy, decimal:'.', miles:','});
-//     },
-//     '':({currenzy = ''}:FormaterCurrencyFnc)=>{
-//         return formaterCurrencystring({currenzy, decimal:'.', miles:','});
-//     }
-// }
-
-// const formaterCurrencystring = ({currenzy, decimal, miles, contry = 'pe'}:any) => {
-//     const numberStr = currenzy.toString();
-  
-//     const part = numberStr.split('.');
-//     const partInt = part[0];
-//     const partFloat = (part[1]?.length == 1 ? `${part[1]}0` : part[1]) || '00'; 
-  
-//     const partIntFormater = partInt.replace(/\B(?=(\d{3})+(?!\d))/g, miles);
-  
-//     return `${ partIntFormater }${partFloat == '' ? '' :  contry == 'cl' ? "" : decimal}${ contry == 'cl' ? '' : partFloat}`;
-// }
-
 export const FormaterCurrency: Record<string, (params: FormaterCurrencyFnc) => string> = {
     'cl': ({ currenzy = '' }: FormaterCurrencyFnc) => {
         const number = parseFloat(currenzy.toString());
