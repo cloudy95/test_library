610375367484173bce351611a18ee7924747eb65
cloudy95 <luissanguino1702@gmail.com>
2025-08-05 11:30:08 -0400

    removiendo ceros a la izquierda en chile

--- src/utils/validaterutsformater.ts
+++ src/utils/validaterutsformater.ts
@@ -20,26 +20,35 @@ export const FormateaRut:any = {
 
 const formaretrutcl = (actual:string = '')=>{
     if (actual !== undefined) {
-        if (actual != "" && actual.length > 1) {
-            var sinPuntos = actual.replace(/\./g, "");
-            var sinEspacios = sinPuntos.replace(/\s/g, "");
-            var actualLimpio = sinEspacios.replace(/-/g, "");
-            var inicio = actualLimpio.substring(0, actualLimpio.length - 1);
-            var rutPuntos = "";
-            var i = 0;
-            var j = 1;
-            for (i = inicio.length - 1; i >= 0; i--) {
-                var letra = inicio.charAt(i);
-                rutPuntos = letra + rutPuntos;
-                if (j % 3 == 0 && j <= inicio.length - 1) {
-                    rutPuntos = "." + rutPuntos;
-                }
-                    j++;
-            }
-            var dv = actualLimpio.substring(actualLimpio.length - 1);
-            rutPuntos = rutPuntos + "-" + dv;
-            return rutPuntos;
+      if (actual != "" && actual.length > 1) {
+        var sinPuntos = actual.replace(/\./g, "");
+        var sinEspacios = sinPuntos.replace(/\s/g, "");
+        var actualLimpio = sinEspacios.replace(/-/g, "");
+        
+        // Elimina ceros al inicio
+        actualLimpio = actualLimpio.replace(/^0+/, '');
+
+        // Si después de limpiar queda vacío, retorna original
+        if (actualLimpio.length < 2) {
+          return actual;
         }
+
+        var inicio = actualLimpio.substring(0, actualLimpio.length - 1);
+        var rutPuntos = "";
+        var i = 0;
+        var j = 1;
+        for (i = inicio.length - 1; i >= 0; i--) {
+            var letra = inicio.charAt(i);
+            rutPuntos = letra + rutPuntos;
+            if (j % 3 == 0 && j <= inicio.length - 1) {
+                rutPuntos = "." + rutPuntos;
+            }
+            j++;
+        }
+        var dv = actualLimpio.substring(actualLimpio.length - 1);
+        rutPuntos = rutPuntos + "-" + dv;
+        return rutPuntos;
+      }
     }
     return actual;
 }
